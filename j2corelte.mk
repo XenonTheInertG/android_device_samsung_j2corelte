b37cf87c96242b0310dc753f2edaf3f16c931c5d
Владислав Сухов <suhov.vlad-04@yandex.ru>
2019-01-28 09:53:44 +0600

    Update j2corelte.mk

--- j2corelte.mk
+++ j2corelte.mk
@@ -1,2 +1,11 @@
+LOCAL_PATH := device/samsung/j2corelte
+
 PRODUCT_COPY_FILES += \
-     device/samsung/j2corelte/prebuilt/dt.img:dt.img
+     $(LOCAL_PATH)/prebuilt/dt.img:dt.img
+
+# Set those variables here to overwrite the inherited values.
+PRODUCT_NAME := full_j2corelte
+PRODUCT_DEVICE := j2corelte
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SM-J260F
